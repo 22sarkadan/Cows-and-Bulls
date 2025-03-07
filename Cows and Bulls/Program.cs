fd38781c6fe7ac2575e129888b93e85f9eaa7a42
22sarkadan <22sarkadan@heckgrammar.co.uk>
2025-03-07 11:20:07 +0000

    Add project files.

---
+++ Cows and Bulls/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Cows_and_Bulls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generates a 4 digit unique number as a char array cos it makes things easier
+            bool Unique = false;
+            Random rand = new Random();
+            char[] Digits = new char[4];
+            while (!Unique)
+            {
+                int numbers = rand.Next(1000, 10000);
+                Digits = numbers.ToString().ToCharArray();
+                Unique = Digits.Distinct().Count() == 4;
+            }
+            //Gets guess, ensures it's correct
+            bool isvalid = false;
+            while (isvalid = false)
+            {
+                Console.WriteLine("Enter you're guess");
+                char[] Guess = Console.ReadLine().ToCharArray();
+                if (Unique = Guess.Distinct().Count() == 4 && Guess.Length == 4)
+                {
+                    isvalid = true;
+                }
+                break;
+            }
+
+
+        }
+    }
+}
